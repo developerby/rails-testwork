d68b31b798f425b91a68f2e0bb31ee670cc6f80a
Sergey Kukunin <sergey.kukunin@gmail.com>
2015-02-22 17:28:05 +0200

    Add test case for nested validation

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe Employee, :type => :model do
   it { should validate_presence_of :company }
   it { should validate_presence_of(:hired_at).on(:strict) }
   it { should belong_to(:company).inverse_of(:employees) }
+
+  describe "nested company validation" do
+    let(:company) { Company.create! name: "Test Company" }
+    let(:employee) { Employee.create! name: "John Smith", company: company, hired_at: 2.days.ago }
+    subject { employee }
+
+    context "with default context" do
+      it { is_expected.to be_valid }
+    end
+
+    context "with strict context" do
+      specify do
+        expect(subject).to_not be_valid(:strict)
+        expect(subject.errors).to include(:'company.founded_at')
+      end
+    end
+  end
 end
